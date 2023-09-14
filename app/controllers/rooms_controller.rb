acbf79661e28bddb898704b4beac3982ae6ab65d
user <user@usernoMacBook-Air.local>
2023-09-14 18:56:18 +0900

    Roomのindexページの表示まで

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,5 +1,28 @@
 class RoomsController < ApplicationController
+  def index
+    @rooms = Room.all
+  end
+
   def new
+    @room = Room.new
+  end
+
+  def show
+    @room = Room.find(params[:id])
+  end
+
+  def create
+    @room = Room.new(params.require(:room).permit(:facility_name, :detail, :price, :address, :image))
+    if @room.save
+      room = Room.find_by(facility_name: @room.facility_name)
+      redirect_to room_path(room.id)
+    else
+      render :new
+    end
+
+    def edit
+    end
     
   end
+
 end
--- db/migrate/20230913140052_image_settings.rb
+++
@@ -1,5 +0,0 @@
-class ImageSettings < ActiveRecord::Migration[6.1]
-  def up
-    add_column :rooms, :image2, :string
-  end
-end
