fba9ff8f833049a8df8fd78bb09ea009cb91e01d
user <user@usernoMacBook-Air.local>
2023-09-17 22:53:31 +0900

    migrationファイルの重複を削除

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_09_17_050512) do
+ActiveRecord::Schema.define(version: 2023_09_16_145055) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
