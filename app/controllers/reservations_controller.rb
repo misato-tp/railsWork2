6ac1cf91649e9da95f723851b78c56d261aa83a4
user <user@usernoMacBook-Air.local>
2023-10-09 22:28:03 +0900

    confirm画面

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -8,26 +8,27 @@ class ReservationsController < ApplicationController
     @reservation = Reservation.new
   end
 
+
+  def confirm
+    @reservation.total_days = ((@reservation.end_date.to_date)-(@reservation.start_date.to_date)).to_i
+    @reservation.total_price = (@reservation.price)*(@reservation.how_many_people)*(@reservation.total_days).to_i
+  end
+
   def create
-    @room = Room.find(params[:room_id])
-    @reservation = Reservation.new(params.require(:reservation).permit(:start_date, :end_date, :how_many_people, :total_price, :room_id))
+    @room = Room.find(params[:reservation][:room_id])
+    @reservation = Reservation.new(reservation_params)
     
     if @reservation.save
-      redirect_to confirm_reservations_path(@room,@reservation)
+      redirect_to confirm_reservations_path(@room, @reservation)
     else
-      render :show
+      render "show"
     end
   end
 
-  def confirm
-  @reservation = Reservation.new(params.require(:reservation).permit(:start_date, :end_date, :how_many_people, :total_days, :total_price, :user_id, :image, :room_id, :price))
-  @room = Room.new(params[:room_id])
-  @reservation.total_days = ((@reservation.end_date.to_date)-(@reservation.start_date.to_date)).to_i
-  @reservation.total_price = (@reservation.price)*(@reservation.how_many_people)*(@reservation.total_days).to_i
-  @reservation.save
-  redirect_to reservations_path
+ private
+  def reservation_params
+    params.require(:reservation).permit(:start_date, :end_date, :how_many_people, :total_price, :room_id)
   end
 
- private
 
 end
