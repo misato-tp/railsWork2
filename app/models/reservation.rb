7a85375d03d04e94657c7077b6e5a36780208719
user <user@usernoMacBook-Air.local>
2023-10-23 00:39:56 +0900

    topの画像トリミングについて

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -5,12 +5,22 @@ class Reservation < ApplicationRecord
     with_options presence: true do
         validates :start_date
         validates :end_date
-        validates :how_many_people
     end
-
+    validates :how_many_people, numericality:{greater_than_or_equal_to: 1}
     validate :start_end_check
+    validate :date_before_start
 
-    def start_end_check
-        errors.add(:end_date, "はチェックイン以降の日付で選択してください。") if end_date.nil? || end_date < start_date
+    def start_end_check 
+        return unless start_date && end_date
+
+        if !end_date.after?(start_date)
+            errors.add(:end_date,"はチェックイン日以降の日付で選択してください。")
+        end
     end
+
+    def date_before_start
+    return if start_date.blank?
+        errors.add(:start_date, "は今日以降のものを選択してください") if start_date < Date.today
+    end
+
 end
